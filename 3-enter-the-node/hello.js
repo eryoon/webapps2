c385763b1fc01ce4140b66329877d12e84bcdde0
Eric <emjyoon@gmail.com>
2016-10-20 14:35:17 -0700

    primary objective achieved!

--- 3-enter-the-node/hello.js
+++ 3-enter-the-node/hello.js
@@ -1,1 +1,22 @@
-console.log("Hello from Node!");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello small world, an infinitely tiny fraction of the great universe, stranded in a corner, forever confined from exploring new frontiers.');
+});
+
+app.listen(3001, function () {
+  console.log('listening on port 3001!');
+
+
+
+  app.get('/', function (req, res) {
+    res.send('Grate you found seecret');
+  });
+
+  app.listen(3002, function () {
+    console.log('listening on port 3002!');
+  });
+
+
+});
